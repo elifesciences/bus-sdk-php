9728739d7a17adad558ac8a0094dc63330b79500
Giorgio Sironi <g.sironi@elifesciences.org>
2017-02-03 16:47:20 +0000

    Bug fix: does not remove elements from the queue
    So queue:watch commands continue to process the same items over and over. Only recommendations is affected

---
+++ tests/src/Command/QueueCommandTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace eLife\Bus\Command;
+
+use eLife\Bus\Monitoring;
+use eLife\Bus\Queue\Mock\WatchableQueueMock;
+use eLife\Bus\Queue\QueueItem;
+use eLife\Bus\Queue\InternalSqsMessage;
+use eLife\Bus\Queue\QueueItemTransformer;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class QueueCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->queue = new WatchableQueueMock();
+        $this->command = new ApplicationSpecificQueueCommand(
+            $this->createMock(LoggerInterface::class),
+            $this->queue,
+            $this->createMock(QueueItemTransformer::class),
+            new Monitoring(),
+            $this->limitIterations(1)
+        );
+        $this->input = $this->createMock(InputInterface::class);
+        $this->output = $this->createMock(OutputInterface::class);
+    }
+
+    public function testDeletesMessagesFromTheQueueAfterProcessing()
+    {
+        $message = new InternalSqsMessage('article', '42');
+        $this->queue->enqueue($message);
+        $this->command->execute($this->input, $this->output);
+        $this->assertEquals(0, $this->queue->count(), "Expected an empty queue");
+    }
+
+    private function limitIterations($number)
+    {
+        $this->iterationCounter = 0;
+        return function() use ($number) {
+            $this->iterationCounter++;
+            if ($this->iterationCounter > $number) {
+                return true;
+            }
+
+            return false;
+        };
+    }
+}
+
+class ApplicationSpecificQueueCommand extends QueueCommand
+{
+    protected function process(InputInterface $input, QueueItem $item, $entity = null)
+    {
+    }
+}
+
